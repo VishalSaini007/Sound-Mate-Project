41ea69aa149aee689efcd2e39552ce82ce61aa8d
vishal saini <vishalsaini9582@gmail.com>
2024-01-30 10:10:44 +0530

    Demo project is dome

---
+++ index.js
@@ -0,0 +1,15 @@
+tailwind.config={
+    darkMode:'class',
+    theme: {
+        extends: {
+         colors:
+          {
+            logo :{
+                '700': '#1C59AE',
+                '800': '#114a9a',
+            }  
+          
+        }
+        }
+    }
+}
